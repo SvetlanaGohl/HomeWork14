04e59dd09225a4270d9c51aa0d0eb5ba459c649e
Svetlana Gohl <sm181183@mail.ru>
2023-07-31 01:50:25 +0300

    ДЗ 14 на проверку

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        public static void main(String[] args) {
+                Bicycle cronus = new Bicycle("Кронус", 2);
+                Bicycle crossM = new Bicycle("Кросс-М", 2);
+
+                cronus.updateTyre();
+
+                Car bmw = new Car("BMW", 4);
+                Car volvo = new Car("VOLVO", 4);
+
+                volvo.checkEngine();
+
+
+                Truck ural = new Truck("Урал", 6);
+                Truck iveco = new Truck("IVECO", 8);
+
+                iveco.checkTrailer();
+
+                ServiceStationImpl serviceStation = new ServiceStationImpl();
+                serviceStation.check(crossM);
+                serviceStation.check(bmw);
+                serviceStation.check(ural);
+
+        }
 }
---
+++ src/ServiceStationImpl.java
@@ -0,0 +1,10 @@
+public class ServiceStationImpl implements ServiceStation {
+    @Override
+    public void check(Transport transport) {
+        System.out.println("Обслуживаем " + transport.getModelName());
+        transport.service();
+    }
+}
+
+
+
